775b25da7c50c134e2071db23b52700ec07f3456
JoeOvenden <joe.ovenden17@gmail.com>
2024-04-10 11:32:02 +0100

    Changes to styling and javascript for /about

--- about/views.py
+++ about/views.py
@@ -1,6 +1,30 @@
+import csv, os
 from django.shortcuts import render
 
 # Create your views here.
 
+class Project_Card:
+    def __init__(self, values):
+        self.title, self.id, self.image_link, self.url, self.description = values
+        self.id = int(self.id)
+
+
+def get_project_cards():
+    cards = []
+    with open('about/templates/about/project-cards-info.csv', newline='') as csvfile:
+        csvreader = csv.reader(csvfile)
+        next(csvreader)     # Skip the header row
+
+        for row in csvreader:
+            card = Project_Card(row)
+            cards.append(card)
+
+    return cards
+
+
 def index(request):
-    return render(request, "about/index.html")
+    project_cards = get_project_cards()
+
+    return render(request, "about/index.html", {
+        "project_cards": project_cards
+    })
